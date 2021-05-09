1826340db11ef2cac1760768ed89cf04ccd3b30f
pixel.fabian <82926714+pixel-fabian@users.noreply.github.com>
2021-05-09 15:03:50 +0200

    :wrench: ADD use clean option from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const debug = process.env.NODE_ENV !== 'production';
 module.exports = {
   entry: './src/js/game.ts',
   mode: debug ? 'development' : 'production',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
   module: {
     rules: [
       {
@@ -18,6 +21,7 @@ module.exports = {
   },
   output: {
     filename: 'game.js',
-    path: path.resolve(__dirname, 'dist/js')
+    path: path.resolve(__dirname, 'dist/js'),
+    clean: true,
   },
 };
